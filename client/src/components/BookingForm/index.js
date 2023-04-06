75e60025a9d0a2b591ec6c4ca3c89763fefc627a
Reed Keyes <rbkeyes@gmail.com>
2023-04-06 13:58:49 -0700

    test-modal works

--- client/src/components/BookingForm/index.js
+++ client/src/components/BookingForm/index.js
@@ -1,25 +1,96 @@
 import React, { useState } from "react";
-import DatePicker from "react-datepicker";
 
+// 🦄 rbk to-do: query service by _id based on service selected to get name of service for modal title 
+// import {useQuery} from '@apollo/client';
+// import { QUERY_SERVICE } from '../utils/queries'
+
+// import datepicker calendar component + css that goes with
+import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+
+// import components from react-bootstrap
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Modal from 'react-bootstrap/Modal';
+
 
 // would be nice to have an icon show up in the input field but maybe just next to it for now?
 // import { FaCalendarDay, FaCalendarWeek} from "react-icons/fa";
 
-export default function Example() {
-    // use state variables for date picker
+export default function BookingForm(props) {
+    // useState for modal
+    const [show, setShow] = useState(false);
+    const handleShow = () => setShow(true);
+    const handleClose = () => setShow(false);
+
+    // useState for date picker
     const [dateRange, setDateRange] = useState([null, null]);
     const [startDate, endDate] = dateRange;
 
+    // 🦄 rbk to-do: function to retrieve service data
+    // const { data } = useQuery(QUERY_USER);
+    // let user;
+
+    // if (data) {
+    //     user = data.user;
+    // };
+
+    // function for form submit 22.22 ./
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+        try {
+          const mutationResponse = await login({
+            variables: { email: formState.email, password: formState.password },
+          });
+          const token = mutationResponse.data.login.token;
+          Auth.login(token);
+        } catch (e) {
+          console.log(e);
+        }
+      };
+    
+      const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFormState({
+          ...formState,
+          [name]: value,
+        });
+      };
+
     return (
-      <DatePicker
-        selectsRange={true}
-        startDate={startDate}
-        endDate={endDate}
-        onChange={(update) => {
-          setDateRange(update);
-        }}
-        isClearable={true}
-      />
+        <>
+            <Button variant="primary" onClick={handleShow}>
+                Book Now
+            </Button>
+
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>
+                        {/* {service.name} */}
+                        {/* add name of selected service as title once models and queries are set */}
+                    </Modal.Title>
+                </Modal.Header>
+                <Form>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+
+                        <DatePicker
+                            selectsRange={true}
+                            startDate={startDate}
+                            endDate={endDate}
+                            onChange={(update) => {
+                                setDateRange(update);
+                            }}
+                            // isClearable={true}
+                            onClickOutside={handleClose}
+                        />
+
+                    </Form.Group>
+                </Form>
+                <Modal.Footer>
+                    <Button variant="secondary">Close</Button>
+                    <Button variant="primary">Save changes</Button>
+                </Modal.Footer>
+            </Modal>
+        </>
     );
 };
--- client/src/pages/Booking.js
+++ client/src/pages/Booking.js
@@ -1,12 +1,23 @@
 import React from "react";
-import BookingForm from "../components/BookingForm";
+// import BookingForm from "../components/BookingForm";
+import TestModal from '../components/TestModal';
+import Button from 'react-bootstrap/Button';
 
 function Booking() {
-    return (
-        <>
-        <BookingForm />
-        </>
-    )
-};
+        const [modalShow, setModalShow] = React.useState(false);
+      
+        return (
+          <>
+            <Button variant="primary" onClick={() => setModalShow(true)}>
+              Launch vertically centered modal
+            </Button>
+      
+            <TestModal
+              show={modalShow}
+              onHide={() => setModalShow(false)}
+            />
+          </>
+        );
+      }
 
 export default Booking;
