4e3a26c3694ee43e52691328ddb2c980f5f4547f
Reed Keyes <rbkeyes@gmail.com>
2023-04-05 20:04:37 -0700

    working on booking form

--- client/src/components/BookingForm/index.js
+++ client/src/components/BookingForm/index.js
@@ -2,4 +2,5 @@
 // import dependencies: react, react-bootstrap elements for form, datepicker
 
 // form component with datepicker
-// fetch available services
+// fetch available services from db and render to form with icon as button?
+// 
