30f6aca3170dc5e1d1b3f0180a2c95db169f3b44
Lian Liu <riceudon@gmail.com>
2023-04-10 21:30:10 -0700

    Updated ProfileEditor.js

---
+++ client/src/components/ProfileEditor/ProfileEditor.js
@@ -0,0 +1,104 @@
+import React, { useState, useEffect } from 'react';
+import { useQuery, useMutation } from '@apollo/client';
+import { QUERY_USER } from '../../utils/queries';
+import { UPDATE_PROFILE } from '../../utils/mutations';
+
+const EditUserProfile = ({ userId }) => {
+    const { loading, error, data } = useQuery(QUERY_USER, {
+      variables: { userId },
+    });
+  
+    const [updateUserProfile, { loading: updateLoading, error: updateError }] = useMutation(
+      UPDATE_PROFILE
+    );
+  
+    // State variables and other code remain the same
+  
+    useEffect(() => {
+      if (data && data.user) {
+        setFirstName(data.user.firstName);
+        setLastName(data.user.lastName);
+        setEmail(data.user.email);
+        setPassword('');
+        setLocation(data.user.location);
+        setAvatar(data.user.avatar);
+      }
+    }, [data]);
+  
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+      try {
+        await updateUserProfile({
+          variables: {
+            userId,
+            profile: {
+              firstName,
+              lastName,
+              email,
+              password,
+              location,
+              avatar,
+            },
+          },
+        });
+        // Show success message or update UI
+      } catch (error) {
+        // Handle error and show error message
+      }
+    };
+  
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error fetching user data</p>;
+  
+    return (
+        <div className="edit-user-profile">
+          <h1>Edit User Profile</h1>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+              placeholder="First Name"
+              required
+            />
+            <input
+              type="text"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+              placeholder="Last Name"
+              required
+            />
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Email"
+              required
+            />
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Password"
+              minLength="5"
+              required
+            />
+            <input
+              type="text"
+              value={location}
+              onChange={(e) => setLocation(e.target.value)}
+              placeholder="Location"
+            />
+            <input
+              type="text"
+              value={avatar}
+              onChange={(e) => setAvatar(e.target.value)}
+              placeholder="Avatar URL"
+            />
+            <button type="submit">Update Profile</button>
+          </form>
+        </div>
+      );
+  };
+  
+  export default EditUserProfile;
