9011e2c5acacccd213fa6c84713eba818e2468ca
mengxue xu <mxu4321@gmail.com>
2023-04-10 18:58:20 -0700

    added comment for mongo atlas

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
 
 // ❄️MX: Connect to MongoDB
+// ❄️MX: mongo atlas uri: https://github.com/Maislinn/Project3_MERN/issues/39
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/petpal-db', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
 module.exports = mongoose.connection;
+
