dd9e73405aa2554bcb575f1fceac571c6c1c389e
Reed Keyes <rbkeyes@gmail.com>
2023-04-11 12:34:49 -0700

    removed modal in favor of simple datepicker for selecting date or date range. Need to add state to render appropriate datepicker and text based on type of product selected

--- client/src/components/SelectDates/index.js
+++ client/src/components/SelectDates/index.js
@@ -2,35 +2,22 @@ import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 
+import DatePicker from "react-datepicker";
+
+import "react-datepicker/dist/react-datepicker.css";
+
 
 function SelectDates() {
-  const [show, setShow] = useState(false);
+  // useState for datepicker
+  // const [placeholder, setPlaceholder] = useState('click to select a date')
+  const [startDate, setStartDate] = useState(null);
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-
-    return (
-      <>
-      <Button variant="primary" onClick={handleShow}>
-        Select Dates
-      </Button>
-
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
-      </Modal>
+  return (
+    <>
+          <p>To book this visit, please select a date below.</p> 
+          <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} placeholderText='click to select a date' />
     </>
-    );
+  );
 }
 
 export default SelectDates;
