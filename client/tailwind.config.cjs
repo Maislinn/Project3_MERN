d0cb14512dccfdbe6791fd6bf40e73bf2b579c47
Reed Keyes <rbkeyes@gmail.com>
2023-04-14 18:23:31 -0700

    installed necessary dependencies for react-tailwind-datepicker and added code to tailwind config

--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [    "./index.html",
-  "./src/**/*.{js,ts,jsx,tsx}",],
+  "./src/**/*.{js,ts,jsx,tsx}",
+  "./src/**/*.{js,jsx,ts,tsx}",
+  "./node_modules/react-tailwindcss-datepicker/dist/index.esm.js",],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
 }
