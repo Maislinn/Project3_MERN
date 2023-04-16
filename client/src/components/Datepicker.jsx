4ada5bef4b88fbc5ae62095e018f952b0d35cf70
Reed Keyes <rbkeyes@gmail.com>
2023-04-15 21:10:05 -0700

    updated datepicker, changed width of input

--- client/src/components/Datepicker.jsx
+++ client/src/components/Datepicker.jsx
@@ -1,76 +1,47 @@
-// import React, {useState} from "react"; 
-// import Datepicker from "react-tailwindcss-datepicker"; 
-// import dayjs from 'dayjs';
+import React, { useState } from "react";
+import Datepicker from "react-tailwindcss-datepicker";
+import dayjs from 'dayjs';
 
 
-// export default function Calendar() { 
+export default function Calendar() {
 
-// const [value, setValue] = useState({ 
+  const [value, setValue] = useState({
+    startDate: null,
+    endDate: null,
+  });
+  // new Date(dayjs(value.startDate).add(3, 'day'))
 
-// startDate: null, 
-// endDate: null,
-// }); 
-// // new Date(dayjs(value.startDate).add(3, 'day'))
-
-// const handleValueChange = (newValue) => {
-// console.log("newValue:", newValue); 
-// setValue(newValue); 
-// } 
-
-// // console.log(dayjs(value.startDate).add(3, 'day'));
-
-// // add code to render single date or range depending on product selected
-// // maybe add something to product db indicating which one
-// return (
-// <Datepicker 
-// // asSingle={true}
-// useRange={false}
-// showShortcuts={true}
-// toggleClassName="absolute bg-pink-300 rounded-r-lg text-white right-0 h-full px-3 text-gray-400 focus:outline-none disabled:opacity-40 disabled:cursor-not-allowed" 
-// // 🔮 enable showFooter for daterange picker only
-// // showFooter={true} 
-// primaryColor={"pink"} 
-// placeholder={"Select date of visit"} 
-// value={value} 
-// displayFormat={"MM/DD/YYYY"} 
-// onChange={handleValueChange} 
-// readOnly={true} 
-
-// /> 
-
-// );
-// }; 
-
-import React, { useState } from 'react';
-
-import { format } from 'date-fns';
-import { DateRange, DayPicker } from 'react-day-picker';
-
-export default function App() {
-  const [range, setRange] = useState(DateRange | undefined);
-
-  let footer = <p>Please pick the first day.</p>;
-  if (range?.from) {
-    if (!range.to) {
-      footer = <p>{format(range.from, 'PPP')}</p>;
-    } else if (range.to) {
-      footer = (
-        <p>
-          {format(range.from, 'PPP')}–{format(range.to, 'PPP')}
-        </p>
-      );
-    }
+  const handleValueChange = (newValue) => {
+    console.log("newValue:", newValue);
+    setValue(newValue);
   }
 
+
+  // console.log(dayjs(value.startDate).add(3, 'day'));
+
+  // add code to render single date or range depending on product selected
+  // render one calendar for start date and second for end date
+  // set min/max dates for end date based on start date selected.
   return (
-    <DayPicker
-      defaultMonth={new Date(2022, 8)}
-      mode="range"
-      min={3}
-      max={6}
-      selected={range}
-      onSelect={setRange}
-      footer={footer}
-    />
+    <div className="w-60"
+    >
+      <Datepicker
+        asSingle={true}
+        useRange={false}
+        showShortcuts={true}
+        toggleClassName="absolute bg-pink-300 rounded-r-lg text-white right-0 h-full px-3 text-gray-400 focus:outline-none disabled:opacity-40 disabled:cursor-not-allowed"
+        // 🔮 enable showFooter for daterange picker only
+        // showFooter={true} 
+        primaryColor={"pink"}
+        placeholder={"Select date"}
+        value={value}
+        displayFormat={"MM/DD/YYYY"}
+        onChange={handleValueChange}
+        readOnly={true}
+        className="w-30"
+      />
+    </div>
+
   );
-}
+};
+
