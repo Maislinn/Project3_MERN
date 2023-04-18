903e94d0fffc7aab426fc7e9eeb83eebf0f4f961
Reed Keyes <rbkeyes@gmail.com>
2023-04-18 13:31:12 -0700

    updated config files for tailwind and vite

--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [    "./index.html",
-  "./src/**/*.{js,jsx,ts,tsx}",
-  "./node_modules/react-tailwindcss-datepicker/dist/index.esm.js",],
+  content: {
+    relative: true,
+    files: [
+      "./index.html",
+      "./pages/*.{js,jsx}",
+      "./components/*.{js,jsx}",
+    ]
+},
   theme: {
     extend: {},
   },
