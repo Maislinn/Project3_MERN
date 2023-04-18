c77c60c78065a69892387318973d95e9913589d3
Reed Keyes <rbkeyes@gmail.com>
2023-04-18 12:55:20 -0700

    updated config

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    optimizeDeps: {
+      include: ['linked-dep'],
+    },
+    build: {
+      commonjsOptions: {
+        include: [/linked-dep/, /node_modules/],
+      },
+    },
   plugins: [react()],
 })
